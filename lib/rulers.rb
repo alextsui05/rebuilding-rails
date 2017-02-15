f4c643ea5e0a3306229c1a14983e3787337c3e99
Alex Tsui <alextsui05@gmail.com>
2017-02-15 11:26:26 -0800

    Add debug output to hello world application

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,8 +3,9 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
+      `echo debug > debug.txt`
       [200, {'Content-Type' => 'text/html'},
-       ["Hello from Ruby on Rulers!"]]
+        ["Hello from Ruby on Rulers!"]]
     end
   end
 end
