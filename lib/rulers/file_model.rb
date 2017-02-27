e154e24d6b080250588a940dfa72f82e378f69de
Alex Tsui <alextsui05@gmail.com>
2017-02-26 22:49:25 -0800

    Add create method to FileModel

--- lib/rulers/file_model.rb
+++ lib/rulers/file_model.rb
@@ -33,6 +33,30 @@ module Rulers
         files = Dir["db/quotes/*.json"]
         files.map { |f| FileModel.new f }
       end
+
+      def self.create(attrs)
+        hash = {}
+        hash["submitter"] = attrs["submitter"] || ""
+        hash["quote"] = attrs["quote"] || ""
+        hash["attribution"] = attrs["attribution"] || ""
+
+        files = Dir["db/quotes/*.json"]
+        names = files.map { |f| f.split("/")[-1] }
+        last_id = names.map(&:to_i).max
+        id = last_id + 1
+
+        File.open("db/quotes/#{id}.json", "w") do |f|
+          f.write <<TEMPLATE
+{
+  "submitter": "#{hash["submitter"]}",
+  "quote": "#{hash["quote"]}",
+  "attribution": "#{hash["attribution"]}"
+}
+TEMPLATE
+        end
+
+        FileModel.new "db/quotes/#{id}.json"
+      end
     end
   end
 end
