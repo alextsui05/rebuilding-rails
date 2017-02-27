89cb0547b884955e947fa6bb4ca798b0283d2607
Alex Tsui <alextsui05@gmail.com>
2017-02-26 20:47:32 -0800

    Add basic file model

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,6 +2,7 @@ require "rulers/version"
 require "rulers/routing"
 require "rulers/dependencies"
 require "rulers/controller"
+require "rulers/file_model"
 
 module Rulers
   class Application
--- lib/rulers/version.rb
+++ lib/rulers/version.rb
@@ -1,3 +1,3 @@
 module Rulers
-  VERSION = "5.1.0"
+  VERSION = "5.2.0"
 end
